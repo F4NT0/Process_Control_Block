8dc1068a10641eefecc463ac0f0c51bba19ba562
F4NT0 <gabriel.stundner@acad.pucrs.br>
2020-07-10 23:25:40 -0300

    Fix Partitions funcionando

---
+++ Fix_Partition/README.md
@@ -0,0 +1,4 @@
+# Process Control Block em Memória de Partições Fixas
+
+* Como as Partições são Fixas, ou seja, a memória é separada em Partições de mesmo tamanho,para cada Processo precisa se saber qual é a primeira posição possivel de memória e qual é a ultima posição que o Processo pode mexer.
+* 
--- Fix_Partition/Solution.java
+++ Fix_Partition/Solution.java
@@ -17,6 +17,18 @@ class Memory {
         }
     }
     public List<Object> getMemory(){return memory;}
+    public int getMemSize(){return size;}
+
+    // Só para definir uma memoria particionada
+    public int getPartition(int numberProcess){
+        switch(numberProcess){
+            case 1: return 8;
+            case 2: return 16;
+            case 3: return 24;
+            case 4: return 32;
+        }
+        return -1;
+    }
 }
 
 class Process{
@@ -36,7 +48,9 @@ class Process{
     public String getState(){return state;}
     public int getPriority(){return priority;}
     public int getBaseRegister(){return baseRegister;}
+    public void setBaseRegister(int value){baseRegister = value;}
     public int getBoundsRegister(){return boundsRegister;}
+    public void setBoundsRegister(int value){boundsRegister = value;}
     public void setPc(int pc){this.pc = pc;}
     public void setState(String state){this.state = state;}
     public void setPriority(int priority){this.priority = priority;}
@@ -44,41 +58,101 @@ class Process{
 
 class PCB{
     Queue<Process> queue;
+    Memory mem;
     public PCB(Memory mem){
         queue = new LinkedList<>();
-        mem.getMemory().add(queue);
-        
-
-    
+        this.mem = mem;
+        mem.getMemory().add(0,queue);    
     }
 
     public void createProcess(int id){
         Process process = new Process(id,"NEW");
         process.setPriority(queue.size());
+        process.setBoundsRegister(mem.getPartition(id));
+        process.setBaseRegister(mem.getPartition(id)-7); // 7 porque são 8 posições por part.
+        process.setPc(process.getBaseRegister());
+        insertOnMemory(process.getBaseRegister(), process.getBoundsRegister(), id);
         queue.add(process);
-        
-        
     }
+
+    public void insertOnMemory(int registerBase, int boundsRegister, int id){
+        int j = 0 ;
+        for(int i = registerBase ; i <= boundsRegister ; i++){
+            String value = "Lendo Linha " + j + " Processo " + id;
+            j++;
+            mem.getMemory().set(i,value);
+        }
+    }
+
+    private int adder(int relative, int baseReg){
+        return relative + baseReg;
+    }
+    private boolean comparator(int value, int boundsReg){
+        if(value <= boundsReg){
+            return true;
+        }
+        return false;
+    }
+
+    public void lerLinha(int valor){
+        Process process = queue.poll();
+        int value = adder(valor, process.baseRegister);
+        if(comparator(value, process.boundsRegister)){
+            System.out.println("Posição " + value + " | Processo " + process.getId() + " : " + mem.getMemory().get(value));
+        }else{
+            System.out.println("Posição " + value + " Não existe no Processo " + process.getId());
+        }
+        queue.add(process);
+    }
+
+    // TESTES
     public int queueSize(){return queue.size();}
     public void testeFila(){
-       while(queue.size() > 0){
-        System.out.println("Processo " + queue.poll().getId()); 
-       }
+       while(queue.size() > 0){System.out.println("Processo " + queue.poll().getId());}
     }
-    
-    
+    public void testeRegisters(){
+        Process aux = queue.poll();
+        System.out.println("Processo " + aux.getId());
+        System.out.println("Base Register: " + aux.getBaseRegister());
+        System.out.println("Bounds Register: " + aux.getBoundsRegister());
+        System.out.println("Prioridade: " + aux.getPriority());
+    }
+
 }
 
 public class Solution{
     public static void main(String[] args){
         Memory mem = new Memory();
         PCB p = new PCB(mem);
-        p.createProcess(4);
-        p.createProcess(2);
+
         p.createProcess(1);
+        p.createProcess(2);
         p.createProcess(3);
-        p.queueSize();
-        p.testeFila();
-        
+        p.createProcess(4);
+
+        //teste da memória adicionando a fila
+        mem.teste();
+        //p.queueSize();
+        //p.testeFila();
+        //mem.teste();
+
+        //teste dos base e bounds registers
+        //p.testeRegisters();
+        // p.testeRegisters();
+        // p.testeRegisters();
+        // p.testeRegisters();
+
+        //posição 5 do Processo 1
+        p.lerLinha(5);
+        // posição 5 do processo 2
+        p.lerLinha(5);
+        // posição 5 do processo 3
+        p.lerLinha(5);
+        // posição 5 do processo 4
+        p.lerLinha(5);
+
+        // Verificar agora se ele retornar para o processo 1
+        p.lerLinha(2);
+
     }
 }
